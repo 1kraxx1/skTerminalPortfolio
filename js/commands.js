36e7b2cd90fcaf1410658da5e974985cdb348896
1kraxx1 <krausovic@gmail.com>
2023-01-21 19:09:59 +0100

    init

---
+++ js/commands.js
@@ -0,0 +1,91 @@
+var password = "sasha";
+var linkedin = "https://www.linkedin.com/in/sasa-krausovic-4a40b762/";
+var github = "https://github.com/1kraxx1?tab=repositories";
+var email = "mailto:krausovic@gmail.com";
+
+whois = [
+  "<br>",
+  "Hello, I'm Sasha!👍",
+  "I'm an aspiring Web Developer with a focus on the MERN stack, but",
+  "still exploring other technologies and frameworks that catch my eye.",
+  "Programing started as an interest, turned into a hoby, and now it is",
+  "something that I really like to do. I'm confident, naturally curious,",
+  "and perpetually working on improving my skills one faceplant problem at a",
+  "time. Right now, working as a full time Support Engineer and a Freelance",
+  "Web Developer, I'm open to job opportunities where I can contribute, learn",
+  "and grow. If I need to define myself in one sentence that would be a",
+  "family man, an animal lover, a sports fan, fishing enthusiast, and",
+  "tech-obsessed.",
+  "<br>",
+];
+
+whoami = [
+  "<br>",
+  "The greatest thing that I have learned is probably the simplest thing any of us can learn: I am who I am.",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/Skraus' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="_blank">github/Skraus' +
+    "</a>",
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you are admin',
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/Skraus' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="_blank">github/Skraus' +
+    "</a>",
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Sasa Krausovic?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Guess the password',
+  '<span class="command">projects</span>       View projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You already know what this does',
+  '<span class="command">email</span>          Email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Sasa Krausovic (Kraus) 2023</span>',
+  "_______________                        |*_/*|________",
+  "|  ___________  |     .-.     .-.      ||_/-_|______  |",
+  "| |           | |    .****. .****.     | |           | |",
+  "| |   0   0   | |    .*****.*****.     | |   0   0   | |",
+  "| |     -     | |     .**Sasha**.      | |     -     | |",
+  "| |   ___/    | |      .*Kraus*.       | |   ___/   | |",
+  "| |___     ___| |       .*****.        | |___________| |",
+  "|_____|_/|_____|         .***.         |_______________|",
+  "  _|__|/ |_|_ .............*.............._|________|_",
+  " / **********                           / ***********  ",
+  "/  ************                        /  ************   ",
+  "-----------------------------------------------------------",
+  '<span class="color2">Welcome to my portfolio web terminal!</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
